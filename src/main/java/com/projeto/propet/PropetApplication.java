cb8d6576cefcd72c3e18108a533b480a90c10239
lucassouza31 <lucaseasy10@gmail.com>
2023-03-15 19:43:18 -0300

    Iniciando projeto

---
+++ src/main/java/com/projeto/propet/PropetApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.propet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/propet/PropetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.propet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
